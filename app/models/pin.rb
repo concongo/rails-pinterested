6cc42281f4d9328a4eff06d438f82b1ea9de22dd
J. Fernando González <jfgonzalezm@gmail.com>
2019-04-04 14:23:08 +0200

    feature: image resize

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,14 @@
 class Pin < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    def self.sizes
+        {
+          thumbnail: { resize_to_limit: [30, 30] },
+          full:     { resize_to_limit: [100, 100] }
+        }
+      end
+    
+    def sized(size)
+        image.variant(Pin.sizes[size]).processed
+    end
 end
